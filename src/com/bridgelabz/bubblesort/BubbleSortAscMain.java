79c774204a3bed3c771ce48e6e852a8f29c29256
8948036201 <azharuddin.ansari108@gmail.com>
2021-12-25 20:29:13 +0530

    Taking user input to sorting in main method

---
+++ src/com/bridgelabz/bubblesort/BubbleSortAscMain.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.bubblesort;
+
+import java.util.Scanner;
+
+public class BubbleSortAscMain {
+
+	static BubbleSortAsc bubbleSortAsc = new BubbleSortAsc();
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the total number of integers to sort: ");
+		int count = scanner.nextInt();
+
+		int[] array = new int[count];
+
+		System.out.print("Enter integers to sort: ");
+		for (int i = 0; i < count; i++) {
+			array[i] = scanner.nextInt();
+		}
+
+		array = bubbleSortAsc.bubbleSortAscending(array);
+
+		for (int element : array) {
+			System.out.println(element);
+		}
+		scanner.close();
+	}
+}
