c74843b6793b9872a7663ab6d45bbb9e6d16ae95
8948036201 <azharuddin.ansari108@gmail.com>
2021-12-25 21:53:50 +0530

    Taking User input as a string in main method

---
+++ src/com/bridgelabz/fifthquestion_mergesortstring/MergeSortMain.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.fifthquestion_mergesortstring;
+
+import java.util.Scanner;
+
+public class MergeSortMain {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter number of strings: ");
+		int count = scanner.nextInt();
+		String[] strings = new String[count];
+
+		System.out.print("Enter Strings to sort: ");
+		for (int i = 0; i < count; i++) {
+			strings[i] = scanner.next();
+		}
+		scanner.close();
+
+		MergeSort mergeSort = new MergeSort();
+		strings = mergeSort.mergeSort(strings);
+
+		System.out.println("Sorted list:");
+		for (String string : strings) {
+			System.out.println(string);
+		}
+	}
+}
