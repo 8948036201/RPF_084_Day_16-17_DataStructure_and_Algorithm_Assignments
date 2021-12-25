90d3e85461ce76a0926b99a42b31425222fe5644
8948036201 <azharuddin.ansari108@gmail.com>
2021-12-25 21:05:13 +0530

    Sorting logic for Bubble Sort in Ascending order

---
+++ src/com/bridgelabz/bubblesortascendingorder/BubbleSortAsc.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.bubblesortascendingorder;
+
+public class BubbleSortAsc {
+
+	public int[] bubbleSortAscending(int[] intArray) {
+
+		int n = intArray.length;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < (n - 1 - i); j++) {
+				if (intArray[j] > intArray[j + 1]) {
+					int temp = intArray[j];
+					intArray[j] = intArray[j + 1];
+					intArray[j + 1] = temp;
+				}
+			}
+		}
+		return intArray;
+	}
+}
