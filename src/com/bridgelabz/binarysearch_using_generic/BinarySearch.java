6614d47a8170f4f80c0d00ef974005451c497c6d
8948036201 <azharuddin.ansari108@gmail.com>
2021-12-26 09:40:42 +0530

    Adding binary search logic using generic

---
+++ src/com/bridgelabz/binarysearch_using_generic/BinarySearch.java
@@ -0,0 +1,33 @@
+package com.bridgelabz.binarysearch_using_generic;
+
+import java.util.Scanner;
+
+public class BinarySearch {
+	static Scanner scanner = new Scanner(System.in);
+	public static <T extends Comparable<T>> int binarySearch(T[] array, T key, int left, int right) {
+
+		if (left > right)
+			return 0;
+
+		int mid = (left + right) / 2;
+
+		if (array[mid].equals(key))
+			return 1;
+		else if ((array[mid]).compareTo(key) > 0)
+			return binarySearch(array, key, left, mid - 1);
+		else
+			return binarySearch(array, key, mid + 1, right);
+	}
+
+	public static <T> void printArray(T[] array) {
+		System.out.println("\n" + "Sorted list :");
+		for (int i = 0; i < array.length; i++)
+			System.out.print(array[i] + " " + "\n");
+
+	}
+	
+	public static int getInteger() {
+		return scanner.nextInt();
+
+	}
+}
