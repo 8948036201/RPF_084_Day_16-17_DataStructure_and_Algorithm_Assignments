83cfa8ec0c9efe9a53471aab22b5393071c75954
8948036201 <azharuddin.ansari108@gmail.com>
2021-12-26 09:16:47 +0530

    logic for insertion sort using generic

---
+++ src/com/bridgelabz/insertionsort_using_generic/InsertionSort.java
@@ -0,0 +1,36 @@
+package com.bridgelabz.insertionsort_using_generic;
+
+import java.util.Scanner;
+
+public class InsertionSort {
+
+	static Scanner scanner = new Scanner(System.in);
+
+	public static <T extends Comparable<T>> T[] insertionSort(T[] array) {
+
+		int i, temporary;
+		for (i = 1; i < array.length; i++) {
+			T key = array[i];
+			temporary = i;
+
+			while (temporary > 0 && array[temporary - 1].compareTo(key) > 0) {
+				array[temporary] = array[temporary - 1];
+				temporary--;
+			}
+			array[temporary] = key;
+		}
+		return array;
+	}
+
+	public static <T> void printArray(T[] array) {
+		System.out.println("\n" + "Sorted list :");
+		for (int i = 0; i < array.length; i++)
+			System.out.print(array[i] + " " + "\n");
+
+	}
+
+	public static int getInteger() {
+		return scanner.nextInt();
+
+	}
+}
